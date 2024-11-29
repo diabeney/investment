8a747746286191538f826c78e988a8eccc507862
Diabene <addodiabene69@gmail.com>
2024-11-29 21:57:12 +0000

    feat(app): remove dist

--- src/sanity/env.ts
+++ src/sanity/env.ts
@@ -1,14 +1,14 @@
 export const apiVersion =
-  process.env.SANITY_API_VERSION || '2024-10-24'
+  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-10-24'
 
 export const dataset = assertValue(
-  process.env.SANITY_DATASET,
-  'Missing environment variable: SANITY_DATASET'
+  process.env.SANITY_STUDIO_DATASET,
+  'Missing environment variable: SANITY_STUDIO_DATASET'
 )
 
 export const projectId = assertValue(
-  process.env.SANITY_PROJECT_ID,
-  'Missing environment variable: SANITY_PROJECT_ID'
+  process.env.SANITY_STUDIO_PROJECT_ID,
+  'Missing environment variable: SANITY_STUDIO_PROJECT_ID'
 )
 
 function assertValue<T>(v: T | undefined, errorMessage: string): T {
